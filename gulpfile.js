cc50ac8e8b18ab220bfe3cef98a7244e30b0d5b9
Matt Gaunt <mattgaunt@google.com>
2020-03-04 19:26:29 -0800

    Minor tweak to support all css files

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('typescript', gulp.series(
 ))
 
 gulp.task('css', gulp.series(
-  css.gulpBuildAll({
+  css.gulpBuild({
     src: themeSrc,
     dst: themeDst,
   }, {
@@ -95,6 +95,7 @@ gulp.task('restart-server', async () => {
 
 gulp.task('watch', 
   gulp.series(
+    'build-into-site',
     'styleguide-theme',
     'styleguide-content',
     gulp.parallel(
